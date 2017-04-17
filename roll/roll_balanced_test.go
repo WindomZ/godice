1e8198a08bb5d340b96e83022b45feaeef79b533
WindomZ <git.windomz@gmail.com>
2017-04-17 13:50:41 +0800

    test: enhance coverage testings

--- roll/roll_balanced_test.go
+++ roll/roll_balanced_test.go
@@ -7,6 +7,9 @@ import (
 
 func Test_RollBalanced_Roll(t *testing.T) {
 	roll := NewBalancedRoll()
+
+	assert.Empty(t, roll.Roll())
+
 	assert.NotEmpty(t,
 		roll.AddRoll(int8(1), 1).AddRoll(int16(2), 2).AddRoll(int32(3), 3).AddRoll(int64(4), 4))
 	assert.NotEmpty(t,
--- roll/string_roll_test.go
+++ roll/string_roll_test.go
@@ -8,6 +8,8 @@ import (
 func Test_Roll_StringRoll(t *testing.T) {
 	roll := NewStringRoll()
 
+	assert.Empty(t, roll.Roll())
+
 	assert.NotEmpty(t,
 		roll.AddRoll(int8(1), 1).AddRoll(int16(2), 2).AddRoll(int32(3), 3).AddRoll(int64(4), 4))
 	assert.NotEmpty(t,
